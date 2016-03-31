fde1c11a9c08a1d65b3ba73b1a2a035bac3095b4
云景 <hqc2010@gmail.com>
2016-03-31 14:05:36 +0800

    新增组推送支持

--- group_push.go
+++ group_push.go
@@ -3,131 +3,72 @@ package xinge
 import (
     "encoding/json"
     "errors"
+    "fmt"
+    "strings"
     "time"
 )
 
 type GroupPushAPI struct {
-    PushType     PushType
-    DeviceToken  string // for single-device push
-    UserAccounts []string
-    Tags         []string
-    TagsOp       string
-    MessageType  MessageType
-    Message      interface{}
-    ExpireTime   int
-    SendTime     time.Time
-    MultiPkgType MultiPkgType
-    PushEnv      PushEnv
-    PlatformType PlatformType
-    LoopTimes    int
-    LoopInterval int
-
-    client *Client // 推送客户端
-    plain  string  // 将要推送的消息
+    client    *Client // 推送客户端
+    messageId uint32  // 消息编号
 }
 
 func NewGroupPushAPI(client *Client) *GroupPushAPI {
     api := GroupPushAPI{
-        client: client,
+        client:    client,
+        messageId: 0,
     }
 }
 
-func (api *GroupPushAPI) CreateMessage() {
-
-}
-
-func (api *GroupPushAPI) Push(devices []string) {
-
-}
-
-func (req *ReqPush) Push() error {
-    var request *Request
-
-    switch req.PushType {
-    case PushType_single_device:
-        request = req.Cli.NewRequest("GET", singleDeviceUrl)
-        request.SetParam("device_token", req.DeviceToken)
-    case PushType_single_account:
-        request = req.Cli.NewRequest("GET", singleAccountUrl)
-        request.SetParam("account", req.UserAccounts[0])
-    case PushType_multi_account:
-        request = req.Cli.NewRequest("GET", multiAccountUrl)
-
-        accounts, err := json.Marshal(req.UserAccounts)
-        if err != nil {
-            return errors.New("<xinge> marshal account list err:" + err.Error())
-        }
-        request.SetParam("account_list", string(accounts))
-    case PushType_all_device:
-        request = req.Cli.NewRequest("GET", allDeviceUrl)
-        //      request.SetParam("loop_times", req.LoopTimes)
-        //      request.SetParam("loop_interval", req.LoopInterval)
-    case PushType_tags_device:
-        request = req.Cli.NewRequest("GET", tagsDeviceUrl)
-
-        tags, err := json.Marshal(req.Tags)
-        if err != nil {
-            return errors.New("<xinge> marshal tag list err:" + err.Error())
-        }
-        request.SetParam("tags_list", string(tags))
-        request.SetParam("tags_op", req.TagsOp)
-        //      request.SetParam("loop_times", req.LoopTimes)
-        //      request.SetParam("loop_interval", req.LoopInterval)
-    default:
-        return errors.New("<xinge> invalid request push type.")
-    }
-
-    request.SetParam("message_type", req.MessageType)
-
-    message := ""
-    switch req.PlatformType {
-    case Platform_android:
-        // message
-        if androidMsg, ok := req.Message.(*AndroidMessage); ok {
-            androidMessage, err := json.Marshal(androidMsg)
-            if err != nil {
-                return errors.New("<xinge> marshal android message err:" + err.Error())
-            }
-
-            message = string(androidMessage)
-        } else {
-            return errors.New("<xinge> invalid android message content.")
-        }
-
-    case Platform_ios:
-        // message
-        if iosMsg, ok := req.Message.(*IosMessage); ok {
-            iosMessage, err := json.Marshal(iosMsg)
-            if err != nil {
-                return errors.New("<xinge> marshal ios message err:" + err.Error())
-            }
-
-            message = string(iosMessage)
-        } else {
-            return errors.New("<xinge> invalid ios message content.")
-        }
-    }
-    request.SetParam("message", message)
-
-    request.SetParam("expire_time", req.ExpireTime)
-    request.SetParam("send_time", req.SendTime.Format("2006-01-03 15:33:34"))
-    // multi_pkg
-    request.SetParam("multi_pkg", req.MultiPkgType)
-    // environment
-    request.SetParam("environment", req.PushEnv)
-
-    response, err := request.Execute()
+func (api *GroupPushAPI) CreateMessage(msgtype MessageType, msgbody interface{}, env PushEnv) error {
+    raw, err := json.Marshal(msgbody)
     if err != nil {
-        return errors.New("<xinge> request err:" + err.Error())
+        return err
     }
 
-    if !response.OK() {
-        return errors.New("<xinge> response err:" + response.Error())
+    req := api.client.NewRequest("POST", createMultiMessageUrl)
+    req.SetParam("message_type", msgtype)
+    req.SetParam("message", string(raw))
+    req.SetParam("expire_time", 1)
+    req.SetParam("multi_pkg", MultiPkg_aid)
+    req.SetParam("environment", env)
+
+    resp, err := req.Execute()
+    if err != nil {
+        return err
     }
+
+    if !resp.OK() {
+        return errors.New("xinge: response err: " + resp.Error())
+    }
+
+    println(fmt.Sprintf("%v", resp.Result))
 
     return nil
 }
 
 func (api *GroupPushAPI) Push(devices []string) error {
+    if api.messageId == 0 {
+        return errors.New("xinge: message id is not set")
+    }
 
+    raw, err := json.Marshal(devices)
+    if err != nil {
+        return err
+    }
+
+    req := api.client.NewRequest("POST", multiDeviceUrl)
+    req.SetParam("device_list", string(raw))
+    req.SetParam("push_id", api.messageId)
+
+    resp, err := req.Execute()
+    if err != nil {
+        return err
+    }
+
+    if !resp.OK() {
+        return errors.New("xinge: response err: " + resp.Error())
+    }
+
+    return nil
 }
